67f6260d276183ca764ec9de2f172432a55d742b
jack <827345030@qq.com>
2022-05-16 00:04:03 +0800

    连接到测试网络同时获取余额

--- app/src/main/java/com/jack/web3j/MainActivity.kt
+++ app/src/main/java/com/jack/web3j/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.jack.web3j
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.jack.web3j.databinding.ActivityMainBinding
-import org.web3j.crypto.WalletUtils
+import org.web3j.protocol.Web3j
 import org.web3j.protocol.Web3jFactory
+import org.web3j.protocol.core.DefaultBlockParameterName
 import org.web3j.protocol.http.HttpService
+import org.web3j.utils.Convert
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var web3j: Web3j
+    // 以太坊节点
+    private val prcUrl = "https://rinkeby.infura.io/v3/f2135104de0a4bd298a2cb62056c5779"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +24,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initWeb3j() {
-        val web3j = Web3jFactory.build(HttpService("https://http-mainnet.hecochain.com"))
+        web3j = Web3jFactory.build(HttpService(prcUrl))
         Thread {
-            val version = web3j.web3ClientVersion().send().web3ClientVersion
+            // 获取版本，可以验证连接是否成功
+//            val version = web3j.web3ClientVersion().send().web3ClientVersion
+
+            // 获取钱包余额
+            val balance = web3j.ethGetBalance("0x50056344225622A6Ab9E0A110E7e51D577Dfc3DA", DefaultBlockParameterName.LATEST)
+                .sendAsync().get().balance
             runOnUiThread {
-                binding.tvVersion.postDelayed({
-                    binding.tvVersion.text = version
-                }, 300)
+                binding.tvVersion.text = "${Convert.fromWei(balance.toString(), Convert.Unit.ETHER)}ETH"
             }
         }.start()
     }
+
 }
